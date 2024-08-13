eb3665b15ebea4d9ba95e82f6ac5eb7f50ffa348
Todd Schavey <tschavey@archer.com>
2024-08-12 20:49:06 -0400

    create `JinjaX Flowbite Dev` debug environment and register jinjax-flowbite components from library

--- demo_app/__init__.py
+++ demo_app/__init__.py
@@ -1,5 +1,9 @@
+import os
 from flask import Flask
 from jinjax import Catalog
+import logging
+logging.basicConfig(level=logging.WARNING)
+logging.getLogger('jinjax').setLevel(logging.DEBUG)
 
 
 app = Flask(__name__)
@@ -19,7 +23,17 @@ app.config['SECRET_KEY'] = '35d092ab79d654448e9041683b53975d8d432cad82ebd009'
 
 # configure JinjaX with the Flask Jinja globals, filters, etc., like `url_for()`
 catalog = Catalog(jinja_env=app.jinja_env)
-catalog.add_folder("demo_app/templates/components")
+
+# add jinjax-flowbite components
+JINJAX_FLOWBITE_DEV_ENV = os.environ.get("JINJAX_FLOWBITE_DEV")
+if JINJAX_FLOWBITE_DEV_ENV is not None and JINJAX_FLOWBITE_DEV_ENV == "1":
+    catalog.add_folder("../jinjax-flowbite/src/jinjax-flowbite")
+else:
+    catalog.add_module("jinjax-flowbite")
+
+# add local jinjax components
+catalog.add_folder("demo_app/jinjax_components")
+
 
 
 import demo_app.webapp
